70a485737fd5bd32fb8e8123487e64e69b0160d2
Zheng JIANG <enzojz@gmail.com>
2023-02-08 02:17:44 +0100

    Void portal tunnel

---
+++ res/config/tunnel/ust_void.lua
@@ -0,0 +1,13 @@
+function data()
+    return {
+        name = _("MENU_MODULE_VOID_TUNNEL"),
+        carriers = { "RAIL", "ROAD" },
+        portals = {
+            { "ust/void.mdl", "ust/void.mdl", "ust/void.mdl" },
+            { "ust/void.mdl", "ust/void.mdl", "ust/void.mdl" },
+            { "ust/void.mdl", "ust/void.mdl", "ust/void.mdl" },
+        },
+        cost = 0.0
+    }
+    end
+    
